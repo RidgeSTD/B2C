c4026c6c5ef0f057d635db8f846ef061733af631
ZhaoGuoquan <zhaoguoquan94@gmail.com>
2014-06-23 17:43:14 +0800

    其他html等文件加入

--- src/Database/DB.java
+++ src/Database/DB.java
@@ -1,5 +1,66 @@
 package Database;
 
-public class DB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.logging.Logger;
 
+public class DB {
+	public static void main(String[] args){
+		
+	}
+	private DB(){
+		try{
+			//TODO connect to m
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/B2C.NB","root","");
+			Statement s=con.createStatement();
+			logger.info("connection set");
+//			 ResultSet rsTables = connection.getMetaData().getTables(null, null, "student", null);  
+//             if(rsTables.next()){  
+//                 logger.info("student table already exsit");  
+//             } else {  
+//            	 Statement s=connection.createStatement();
+//                 s.executeUpdate("create table student (sid int,name text,gender text,age int,classNum int,password text,birthDate date);");
+//                 logger.fine("create student success");
+//                 
+//             }  
+//             rsTables.close();
+//             rsTables = connection.getMetaData().getTables(null, null, "Admin", null);  
+//             if(rsTables.next()){  
+//                 logger.info("admin table already exsit");  
+//             } else {  
+//            	 Statement s=connection.createStatement();
+//                 s.executeUpdate("create table admin (aid int,password text);");
+//                 s.executeUpdate("insert into admin values(1,'admin')");
+//                 logger.fine("create admin success");
+//             }  
+//             rsTables.close();
+//             rsTables = connection.getMetaData().getTables(null, null, "score", null);  
+//             if(rsTables.next()){  
+//                 logger.info("score table already exsit");  
+//             } else {  
+//            	 Statement s=connection.createStatement();
+//                 s.executeUpdate("create table score (aid int,className text,score float);");
+////                 s.execute("insert into admin values(1,'admin')");
+//                 logger.fine("create socre success");
+//             }  
+//             rsTables.close();
+//             connection.close();
+             
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static DB getInstance(){
+		return db;
+	}
+	private static DB db=new DB();
+	private Connection connection;
+//	private PreparedStatement preparedStatement;
+	private Logger logger;
 }
