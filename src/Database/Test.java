9bdbcd61acb2aad5710981b843268a503936af0c
ZhaoGuoquan <zhaoguoquan94@gmail.com>
2014-06-26 23:42:05 +0800

    提供了便捷的数据库建表方法，sql文件改变

--- src/Database/Test.java
+++ src/Database/Test.java
@@ -1,14 +1,17 @@
 package Database;
 
+import java.sql.PreparedStatement;
+
 import model.NBOrder;
 
 public class Test {
 	public static void main(String[]args){
 		DB db=DB.getInstance();
 //		NBOrder order=new NBOrder(orderID, userID, orderDate, state, scoreGet, userAddressID)
-		NBOrder order =new NBOrder(1, 1, new java.util.Date(2014-1900,6,25), 0, 0, 1, 100.0);
+//		NBOrder order =new NBOrder(1, 1, new java.util.Date(2014-1900,6,25), 0, 0, 1, 100.0);
 		System.out.println(order);
 //		Integer a=db.insertNBOrder(order);
 //		System.out.println(a);
+		
 	}
 }
