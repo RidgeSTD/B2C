d74d63df464db2be611fc1a8cd7e1e9cde718575
ZhaoGuoquan <zhaoguoquan94@gmail.com>
2014-06-26 22:42:03 +0800

    insert方法返回值全部更改为相应对象，文档升级至2.0

--- src/Database/Test.java
+++ src/Database/Test.java
@@ -8,7 +8,7 @@ public class Test {
 //		NBOrder order=new NBOrder(orderID, userID, orderDate, state, scoreGet, userAddressID)
 		NBOrder order =new NBOrder(1, 1, new java.util.Date(2014-1900,6,25), 0, 0, 1, 100.0);
 		System.out.println(order);
-		db.insertNBOrder(order);
-		
+//		Integer a=db.insertNBOrder(order);
+//		System.out.println(a);
 	}
 }
